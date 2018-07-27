1c70f1fb06b816a016b9f8d933433704b6416b9f
Mikhail Efremov <sem@altlinux.org>
2018-07-27 18:30:46 +0300

    Revert "Don't limit the reneg_bytes value with GOST ciphers"
    It should be done in other way.
    This reverts commit 32ec21399a8fc9f346cc276f4923186e23de07c6.

--- src/openvpn/ssl.c
+++ src/openvpn/ssl.c
@@ -1992,9 +1992,8 @@ tls_session_generate_data_channel_keys(struct tls_session *session)
         msg(D_TLS_ERRORS, "TLS Error: generate_key_expansion failed");
         goto cleanup;
     }
-    if (!strstr(cipher_kt_name(session->opt->key_type.cipher), "gost"))
-        tls_limit_reneg_bytes(session->opt->key_type.cipher,
-                              &session->opt->renegotiate_bytes);
+    tls_limit_reneg_bytes(session->opt->key_type.cipher,
+                          &session->opt->renegotiate_bytes);
 
     ret = true;
 cleanup:
